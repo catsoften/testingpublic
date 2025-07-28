8d1c99504651d7cb6a8aedbdedac9ed3744aabcf
catsoften <catsoften@catsoften.net>
2025-07-27 21:13:09 -0400

    [t] zip_root_real and perl testing

--- .github/build.sh
+++ .github/build.sh
@@ -137,7 +137,7 @@ if [[ -z ${BSH_NO_PACKAGES-} ]]; then
 			pacman -S --noconfirm --needed mingw-w64-ucrt-x86_64-{gcc,cmake,make,ninja,7zip} patch
 		fi
 		if [[ $BSH_HOST_ARCH == x86_64 ]]; then
-			choco install nasm
+			choco install nasm strawberryperl
 		fi
 		;;
 	darwin)
@@ -1136,7 +1136,7 @@ function compile_libwebpmux()
 
 function compile_x264()
 {
-	which perl
+	pkg-config --list-all
 	get_and_cd x264-r3222-b35605a.tar.gz x264_version
 	local configure=./configure
 	configure+=$'\t'--enable-pic
@@ -1177,6 +1177,7 @@ function compile_x264()
 		$configure
 	fi
 	make install-lib-shared -j$NPROC
+	ls -R $zip_root_real
 
 	echo 32b1062f7da84967e7019d01ab805935caa7ab7321a7ced0e30ebe75e5df1670 COPYING | sha256sum -c
 	cp COPYING $zip_root_real/licenses/libx264.LICENSE
@@ -1186,6 +1187,9 @@ function compile_x264()
 
 function compile_ffmpeg()
 {
+	pkg-config --list-all
+	PATH=C\:/Strawberry/perl/bin/:$PATH
+	which perl
 	get_and_cd ffmpeg-7.1.tar.gz ffmpeg_version
 	local configure=./configure
 	configure+=$'\t'--prefix=$zip_root_real
@@ -1224,7 +1228,7 @@ function compile_ffmpeg()
 	configure+=$'\t'--enable-protocol=file
 	configure+=$'\t'--disable-devices
 	configure+=$'\t'--disable-filters
-	PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$zip_root_real/lib/pkgconfig" $configure
+	$configure
 	make install -j$NPROC
 	echo 8177f97513213526df2cf6184d8ff986c675afb514d4e68a404010521b880643 COPYING.GPLv2 | sha256sum -c
 	cp COPYING.GPLv2 $zip_root_real/licenses/ffmpeg.LICENSE
