a89bb7d67d945140af000eabdd89d5c737edfc6b
catsoften <catsoften@catsoften.net>
2025-10-08 01:59:09 -0400

    Test new libwebp with all build types

--- .github/build.sh
+++ .github/build.sh
@@ -32,7 +32,7 @@ tarball_hash() {
 	jsoncpp-1.9.5.tar.gz)      sha256sum=f409856e5920c18d0c2fb85276e24ee607d2a09b5e7d5f0a371368903c275da2;; # acquired from https://github.com/open-source-parsers/jsoncpp/archive/refs/tags/1.9.5.tar.gz
 	bzip2-1.0.8.tar.gz)        sha256sum=ab5a03176ee106d3f0fa90e381da478ddae405918153cca248e682cd0c4a2269;; # acquired from https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz
 	nghttp2-1.50.0.tar.gz)     sha256sum=6de469efc8e9d47059327a6736aebe0a7d73f57e5e37ab4c4f838fb1eebd7889;; # acquired from https://github.com/nghttp2/nghttp2/archive/refs/tags/v1.50.0.tar.gz
-	libwebp-1.3.0.tar.gz)      sha256sum=64ac4614db292ae8c5aa26de0295bf1623dbb3985054cb656c55e67431def17c;; # acquired from https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.3.0.tar.gz
+	libwebp-1.6.0.tar.gz)      sha256sum=e4ab7009bf0629fd11982d4c2aa83964cf244cffba7347ecd39019a9e38c4564;; # acquired from https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.6.0.tar.gz
 	*)                                         >&2 echo "no such tarball (update tarball_hash)" && exit 1;;
 	esac
 }
@@ -952,7 +952,7 @@ function compile_bzip2() {
 
 function compile_libwebpmux()
 {
-	get_and_cd libwebp-1.3.0.tar.gz libwebpmux_version
+	get_and_cd libwebp-1.6.0.tar.gz libwebpmux_version
 	mkdir build
 	cmake_configure=cmake # not local because add_*_flags can't deal with that
 	cmake_configure+=$'\t'-DWEBP_BUILD_ANIM_UTILS=OFF
